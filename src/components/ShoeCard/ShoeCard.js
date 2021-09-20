ad24037587f8404a1a147cb0335f12142d1b916d
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-09-20 15:04:03 +0200

    feat: implement Shoe Grid (ex. 4A)

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,6 +53,9 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+
+  flex-basis: 200px;
+  flex-grow: 1;
 `;
 
 const Wrapper = styled.article``;
@@ -61,10 +64,16 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
 `;
 
 const Name = styled.h3`
